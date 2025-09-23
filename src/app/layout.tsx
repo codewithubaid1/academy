a7fd0af14eaefc472e76da0633a779d0d372c805
codewithubaid1 <boyu6892@gmail.com>
2025-09-23 11:27:40 -0700

    google verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,27 +6,30 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: {
-    default: 'Academic Coaching Center',
-    template: '%s | Academic Coaching Center',
+    default: 'Alhamd academic Coaching Center',
+    template: '%s | Alhamd Academic Coaching Center',
   },
   description: 'Evening Coaching and Mathematics Classes by expert teachers.',
   keywords: [
-    'Academic Coaching Center',
+    'Aphamd academic Coaching Center',
     'Evening Classes',
     'Math Classes',
     'Coaching Institute',
     'Tutoring Burewala',
   ],
   openGraph: {
-    title: 'Academic Coaching Center',
+    title: 'Alhamd academic Coaching Center',
     description: 'Evening Coaching and Mathematics Classes by expert teachers.',
     url: 'https://alhamd-five.vercel.app',
-    siteName: 'Academic Coaching Center',
+    siteName: 'Alhamd academic Coaching Center',
     locale: 'en_US',
     type: 'website',
   },
   alternates: {
     canonical: 'https://alhamd-five.vercel.app',
+  },
+   verification: {
+    google: 'oWxWi0NX3MaAq0qFtrd_e_tZyKJ3VWG7s-8z6mmD0zo',
   },
 }
 
